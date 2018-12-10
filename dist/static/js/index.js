5ddd738aa5f02d5100a055cf1078aa306d82f92a
hannahjx <2278636214@qq.com>
2018-12-10 21:55:54 +0800

    添加product页面及scss一线部分

---
+++ dist/static/js/index.js
@@ -0,0 +1,119 @@
+//头部（导航条）区域
+var nav_swiper=(function(){
+    return{
+        init(ele){
+            this.$nav=$(ele);
+            this.$logo=this.$nav.find('.logo');
+            this.$logo1=this.$nav.find('.logo1');
+            this.$nav_list=$('.nav_list');
+            this.$hover_list=$('.hover_list');
+            this.$ul_bg=$('.ul_bg');
+            this.$search=$('.search');
+            this.$icon_wd=$('.icon-wd');
+            this.$icon_gwc=$('.icon-gwc');
+            // this.liAll=this.$nav_list.find('li');
+            this.event();
+        },
+        event(){
+            var _this=this;
+            this.$nav_list.on('mouseenter','li',function(){
+                _this.$logo.css('display','none');
+                _this.$logo1.css('display','block');
+                _this.$search.css('border','solid 1px #999');
+                _this.$icon_wd.css('color','#000');
+                _this.$icon_gwc.css('color','#999');
+                _this.$icon_gwc.find('i').css('color','#fff');
+                if($(this).attr('class')== 'APPstor'){
+                    _this.$ul_bg.css('height',274);
+                }else{
+                    _this.$ul_bg.css('height',274);
+                }
+                console.log(_this.$hover_list.eq(4));
+                _this.$nav_list.find('a').css('color','#000');
+                _this.$hover_list.find('a').css('color','#000');
+                $(this).find('a').css('color','#00C3F5');
+                $(this).find('ul').css('height',274);
+                $(this).find('.hover_list li').css({'display':'block','zIndex':99});
+                $(this).siblings().find('.hover_list li').css({'display':'none','zIndex':1});
+            })
+            this.$ul_bg.on('mouseleave',function(){
+                _this.$logo1.css('display','none');
+                _this.$logo.css('display','block');
+                _this.$search.css('border','solid 1px #fff');
+                _this.$icon_wd.css('color','#fff');
+                _this.$icon_gwc.css('color','#fff');
+                _this.$ul_bg.css('height',0);
+                _this.$nav_list.find('a').css('color','#fff');
+                _this.$hover_list.css('height',0);
+                _this.$hover_list.find('a').css('color','#000');
+                $(this).find('.hover_list li').css('display','none');
+            })
+        }
+    }
+}())
+nav_swiper.init('.nav');
+//banner区域
+var banner_swiper=(function(){
+    var timer = null;
+    var showWidth = null;
+    return{
+        init(ele){
+            this.showIndex=0;
+            this.$banners=$(ele);
+            this.$lunbo=this.$banners.children('.lunbo');
+            this.$imgAll=this.$lunbo.find('li');
+            this.$btns=$('.banner_btn').find('li');
+            console.log(this.$btns);
+            //克隆
+            this.firstimg= this.$imgAll.first();
+            this.lastimg=this.$imgAll.last();
+            this.$lunbo.append(this.firstimg.clone().css("zIndex",1));
+            this.$lunbo.prepend(this.lastimg.clone().css("zIndex",8));
+            showWidth=this.$banners.width();
+            this.$lunbo.css('left',-showWidth);      
+            this.event();
+            this.autoPlay(this.index);
+        },
+        event(){
+            var _this=this;
+            // _this.$btns.on('click','li',function(){
+			// 	_this.showIndex=$(this).index();
+			// 	console.log(_this.showIndex);
+			// 	// _this.showImages(showIndex);
+			// 	// _this.autoPlay(showIndex);
+            // })
+            this.$btns.on('click', function () {
+                var index = $(this).index();
+                // clearInterval(timer)
+                console.log(index);
+                _this.showImage(index);
+                _this.index = index;
+                _this.autoPlay(index);
+            })	
+        },
+        showImage: function (index = 0) {
+            if (index >= this.$btns.length) {
+                index = 0;
+                this.$lunbo.css('left', 0);
+            } else if (index < 0) {
+                index = this.$tipBoxLiALl.length - 1;
+                this.$lunbo.css('left', -showWidth * (index + 2));
+            }
+            this.index = index;
+            this.$btns.eq(index).addClass('btns').siblings().removeClass('btns');
+            this.$lunbo.animate({
+                left: -showWidth * (index + 1)
+            }, 800)
+        },
+        autoPlay(index){
+            var _this = this;
+            this.index = index || 0
+            clearInterval(timer);
+            timer = setInterval(function(){
+                _this.index++               
+                _this.showImage(_this.index);
+            },5000)
+        }
+    }
+}())
+banner_swiper.init('.banners');
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,107 @@
-var gulp = require('gulp');
+const gulp = require('gulp');//基础库,全局gulp
+const babel = require('gulp-babel');//转义
+const concat= require('gulp-concat'); //文件合并
+const watch= require ('gulp-watch') ; //监听文件
+const minihtml = require('gulp-htmlmin');//压缩HTML文件
+const uglify=require('gulp-uglify');//压缩js文件
+const cleanCSS = require('gulp-clean-css');//压缩css文件
+const imagemin = require('gulp-imagemin');//压缩图片
+const connect = require('gulp-connect');//服务器
+const rev = require('gulp-rev');//生成hash后缀
+const del = require('del');//删除文件
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const clean = require('gulp-clean');//清除文件夹
+const runSequence = require('run-sequence');//异步改同步执行
 
-gulp.task('default', function() {
-  // 将你的默认的任务代码放在这
-  console.log(1+2);
+//压缩html
+// gulp.task('minihtml',function(){
+//     gulp.src('app/**/*.html')//找到文件
+//     .pipe(concat('app/**/*.html'))//合并文件
+//     .pipe(babel({
+//         presets: ['@babel/env']//转义
+//     }))
+//     .pipe(rev())//生成hash后缀
+//     .pipe(htmlmin({ collapseWhitespace: true }))//压缩
+//     .pipe(gulp.dest('dist'))//输出
+//     .pipe(connect.reload());//热更新
+// })
+//压缩css
+// gulp.task('cleanCSS',function(){
+//     gulp.src('app/static/css/*.css')
+//     .pipe(concat('app/static/css/*.css'))//合并文件
+//     .pipe(babel({
+//         presets: ['@babel/env']
+//     }))
+//     .pipe(rev())
+//     .pipe(cleanCSS({compatibility: 'ie8'}))
+//     .pipe(gulp.dest('dist'))
+//     .pipe(connect.reload());
+// })
+//压缩js
+// gulp.task('minijs',function () {
+//     gulp.src('app/static/js/*.js')
+//     .pipe(concat('app/static/js/*.js')) 
+//     .pipe(babel({
+//         presets: ['@babel/env']
+//     }))
+//     .pipe(rev())
+//     .pipe(uglify())
+//     .pipe(gulp.dest('dist'))
+//     .pipe(connect.reload());
+//   })
+//压缩图片
+// gulp.task('imagemin',function () {
+//     gulp.src('app/static/images/*.js')
+//     .pipe(concat('app/static/images/*.js')) 
+//     .pipe(babel({
+//         presets: ['@babel/env']
+//     }))
+//     .pipe(rev())
+//     .pipe(imagemin())
+//     .pipe(gulp.dest('dist'))
+//     .pipe(connect.reload());
+//   })
+//清除文件
+// gulp.task('del', function () {
+//     del('dist','rev');
+// })
+//异步改同步执行
+// gulp.task('build', function(callback) {
+    //     runSequence('del',
+    //         ['minicss', 'minijs'],
+    //         'minihtml',
+    //         callback);
+    //   });
+    gulp.task('del', function () {
+        del('dist','rev');
+    })
+//监听文件
+gulp.task('devwatch',function(){
+    gulp.watch('app/**/*.*',['all','sass'])
+})
+//开启服务器
+gulp.task('connect',function(){
+    connect.server({
+        root: 'dist',
+        port: '7777',
+        livereload: true
+    });
+})
+gulp.task('all',function () {
+    gulp.src('app/**/*.*')
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload())
+})
+gulp.task('sass', function () {
+    gulp.src('app/static/css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
 });
-gulp.task('minihtml',function(){
-    console.log('我要压缩html');
+gulp.task('dev',function () {
+    runSequence('del','all','sass','connect','devwatch')
 })
-gulp.task('minicss',function(){
-    console.log('我要压缩css');
-})
-gulp.task('minijs',function(){
-    console.log('我要压缩js');
-})
-gulp.task('mini',['minihtml','minicss','minijs'],function(){
-    console.log('压缩成功');
-})
+  //开发环境使用
+// gulp.task('dev', ['clean','all','watch','connect']);
+//正式环境使用
+// gulp.task('proud', ['clean','all','watch','connect']);
