89ad3d322700008194b0bd76a8461ef75c16d024
qizhi522 <l>
2018-12-13 14:52:10 +0800

    详情页功能完成 可数据传输

--- dist/static/js/details.js
+++ dist/static/js/details.js
@@ -1,6 +1,7 @@
 var xuanze = (function(){
     return {
         init(ele) {
+            
             this.$ele = $(ele);
             this.$xh =this.$ele.find('dl');
             this.$xhbox = this.$xh.find('dd');
@@ -8,26 +9,75 @@ var xuanze = (function(){
             this.$reduce = this.$xh.last().find('.reduce');
             this.$num = this.$xh.last().find('input');
             this.$push = this.$xh.last().find('.push');
+            this.$xzxh = $('.xhbox .box-active');
+            this.name = this.$xzxh.html();
+            this.$carbtn = $('.car-btn');
+            this.data = []; 
             this.event();
         },
         event() {
             var _this = this;
             this.$xhbox.on('click','div',function(){
                 $(this).addClass('box-active').siblings().removeClass();
+                _this.$xzxh = $('.xhbox .box-active');
+                _this.name = _this.$xzxh.html();               
             })
             this.$reduce.on('click',function(){
                 let num = Number(_this.$num.val());
                 num --;
-                if(num <= 1){
-                    num = 1;
+                if(num <= 0){
+                    num = 0;
                 }
                 _this.$num.val(num);
+                _this.getData();
             })
             this.$push.on('click',function(){
                 let num = Number(_this.$num.val());
                 num ++;
                 _this.$num.val(num);
+                _this.getData();
             })
+            this.$carbtn.on('click',function(){
+                $('.alert').css('display','flex');
+            })
+            $('.car-close').on('click',function(){
+                $('.alert').css('display','none');
+            })
+        },
+        getData() {
+            var _this = this;
+            $.ajax({
+                url: "static/json/data.json",
+                success: function (res) {
+                    for(var i = 0;i<res.length;i++){
+                        if(res[i].name == _this.name){
+                            res[i].count = Number($('.sl').val());
+                            _this.data.pop();
+                            _this.data.push(res[i]);
+                        }
+                    }
+                    console.log(_this.data);
+                    _this.setItem(_this.data);
+                }
+            });
+        },
+        setItem(data) {
+            
+            var shopList = localStorage.getItem('shopList') || '[]';
+            shopList = JSON.parse(shopList);
+            // debugger
+            for(var i = 0;i<shopList.length;i++){
+                console.log(shopList[i][0].id);
+                if(data[0].id == shopList[i][0].id){
+                    shopList[i][0].count = data[0].count;
+                    break;
+                }
+            }
+            if(i == shopList.length){
+                shopList.push(data);
+            }
+            localStorage.shopList = JSON.stringify(shopList);
+            console.log(shopList);
         }
     }
 }());
@@ -49,4 +99,19 @@ var stick = (function(){
             })
         }
     }
-}()) 
+}());
+
+
+var shop = (function(){
+    return {
+        init() {
+            this.$xh = $('.xhbox .box-active');
+            // console.log(this.$xh);
+            this.event();
+        },
+        event() {
+            var _this = this;
+            
+        }
+    }
+}())
